53543edc52c361d43125a5f0ca74645ab80e4d2b
Shaurya Pathak <shauryapathak24@gmail.com>
2020-06-25 11:52:00 -0700

    Initial Commit

---
+++ Tangent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tangent
+//
+//  Created by Shaurya Pathak on 6/25/20.
+//  Copyright © 2020 Tangent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
